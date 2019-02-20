5febc474397ea751a0b0462ef147e31f6aa69e29
amulx <648303666@qq.com>
2019-02-20 10:18:12 +0800

    查询语句优化

--- src/main/java/MyIndex.java
+++ src/main/java/MyIndex.java
@@ -9,33 +9,25 @@ import org.jooq.impl.DSL;
 import java.sql.SQLException;
 
 import static DB.gen.Tables.SHOP_ADMIN;
-import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.select;
 
 public class MyIndex {
     public static void main(String[] args) throws SQLException {
-        // org.jooq.codegen.GenerationTool.main(new String[]{"/home/amu/IdeaProjects/amu.xml"});
 
         DSLContext dslcontext = DSL.using(DBUtil.getConnection(), SQLDialect.MYSQL);
-        // 方法一 直接执行sql
-        /*
-        Result<Record> records = dslcontext.fetch("select * from shop_admin");
-        for (Record r : records) {
-            System.out.println(r.get("adminuser"));
-        }
-        */
-        // 方法二：
-        /*
-         Result<Record> records = dslcontext.select()
-                .from("shop_admin")
-                .orderBy(field("adminid")).fetch();
 
-        */
-        // 方法三：
+        int pageSize=10;
+        int page = 2;
+        // 查询语句优化
+        // select * from shop_admin where adminid <= (select adminid from shop_admin order by id desc limit 1 offset 20) order by id desc limit 10
         Result<Record> records = dslcontext.select()
                 .from(SHOP_ADMIN)
-                .orderBy(SHOP_ADMIN.ADMINID).fetch();
+                .where(SHOP_ADMIN.ADMINID.le(select(SHOP_ADMIN.ADMINID).from(SHOP_ADMIN)
+                        .orderBy(SHOP_ADMIN.ADMINID.desc()).limit(1).offset(page*pageSize)))
+                .orderBy(SHOP_ADMIN.ADMINID.desc()).fetch();
 
          for (Record r : records){
+//             r.valuesRow();
              System.out.println(r.get("adminuser"));
          }
 
